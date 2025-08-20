334fcc0eeee0240e74ed992f2cdbda36cc7e4266
moumith-bingo <moumith.uki.stu@dreamspace.academy>
2025-08-20 16:11:44 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ var name1="nina";
 var name2="mano";
 var name3="sunmi";
 var name4="nithan";
-var name="mithu";
+var name5="mithu";
 
  function PrintName(){
     console. log("Hello "+name1);
@@ -11,6 +11,15 @@ var name="mithu";
     console. log("Hello "+name4);
 }
 
-    console. log(name)
-    console. log(typeof(name))
+    console. log(name5)
+    console. log(typeof(name5))
     PrintName()
+
+    function print(name,country,age) {
+        console.log("hello "+name);
+        console.log("you are from "+country);
+        console.log("you are " +age);
+        
+    }
+    print("moumith","srilanka",20);
+
