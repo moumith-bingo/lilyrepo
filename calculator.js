9ac9cf6b330bc5a8c95a3bb55f6620986dc8c7cc
moumith-bingo <moumith.uki.stu@dreamspace.academy>
2025-08-22 14:05:16 +0530

    kokokok

---
+++ calculator.js
@@ -0,0 +1,32 @@
+var num1= (5)
+var num2= (10)
+var num3= (30)
+var num4= (48)
+
+
+
+function sum(x,y) {
+    console.log(x+y)
+  
+}
+
+function minus(x,y) {
+
+    console.log(x-y)
+
+}
+function reduce(x,y) {
+
+    console.log(y/x)
+
+}
+
+function up(x,y) {
+    console. log(x*y)
+    
+}
+sum(num4,num2)
+minus(num3,num2)
+reduce(num4,num2)
+up(num1,num4)
+
---
+++ fruit.js
@@ -0,0 +1,32 @@
+var a = ("apple");
+var b = ("banana");
+var c = ("orange");
+
+var qtyapple = 40;
+var qtybanana = 100;
+var qtyorange = 80;
+
+const applePrice = 2.50;
+const bananaPrice = 1.20;
+const orangePrice = 1.80;
+
+let cust1 = (2*applePrice)+ (3*bananaPrice)+ (1*orangePrice);
+let cust2 = (1*applePrice) + (2*bananaPrice)+ (4*orangePrice);
+let cust3 = (5*applePrice) + (2*orangePrice);
+
+console. log(cust1);
+console. log(cust2);
+console. log(cust3);
+
+console. log("customer 1 total : $"+cust1);
+console. log("customer 2 total : $"+cust2);
+console. log("customer 3 total : $"+cust3);
+
+console. log(typeof(10))
+console. log(typeof(applePrice))
+console. log(typeof(qtyapple))
+console. log(typeof(a))
+var num=9
+console. log(typeof(num))
+var count=null;
+console. log(typeof(count))
